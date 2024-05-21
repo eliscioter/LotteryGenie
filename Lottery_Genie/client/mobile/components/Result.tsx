2a4b70bfc51e30a5e9413a01955b73c3a4353036
eliscioter~ <attila433lehun@gmail.com>
2024-05-21 21:47:37 +0800

    style: Add peso symbol

--- Lottery_Genie/client/mobile/components/Result.tsx
+++ Lottery_Genie/client/mobile/components/Result.tsx
@@ -5,7 +5,7 @@ import { index_styles } from "@/assets/stylesheets/index";
 export default function Result() {
   return (
     <View style={index_styles.result_container}>
-      <Text style={{ color: Colors.light_grey }}>
+      <Text style={{ color: Colors.light_grey, textAlign: "center"}}>
         You got 5 out of 6. Congratulations,You won!
       </Text>
       <View style={index_styles.winning_combinations_container}>
@@ -28,7 +28,7 @@ export default function Result() {
           <Text style={index_styles.white_text}>10</Text>
         </View>
       </View>
-      <Text style={index_styles.prize_won_text}>You won P 15,000,000</Text>
+      <Text style={index_styles.prize_won_text}>You won {'\u20B1'} 15,000,000</Text>
     </View>
   );
 }
