39a28e724aa7fabb7eec9f1f44ebd7bfe6be5e3e
eliscioter~ <attila433lehun@gmail.com>
2024-05-21 21:45:32 +0800

    feat: change to ScrollView

--- Lottery_Genie/client/mobile/app/(tabs)/index.tsx
+++ Lottery_Genie/client/mobile/app/(tabs)/index.tsx
@@ -3,16 +3,17 @@ import CurrentJackpotPrize from "@/components/CurrentJackpotPrize";
 import InputForm from "@/components/InputForm";
 import Result from "@/components/Result";
 import { Text, View } from "@/components/Themed";
+import { ScrollView } from "react-native";
 
 export default function TabOneScreen() {
   return (
-    <View style={index_styles.container}>
+    <ScrollView style={index_styles.container}>
       <Text style={index_styles.title}>{new Date().toLocaleDateString()}</Text>
       <CurrentJackpotPrize />
       <View style={index_styles.input_form_container}>
         <InputForm />
         <Result />
       </View>
-    </View>
+    </ScrollView>
   );
 }
