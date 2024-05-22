f39727e293a4d0c738fdd1500dc5d1b3a9546e7e
eliscioter~ <attila433lehun@gmail.com>
2024-05-22 21:09:03 +0800

    refractor: Remove params

--- Lottery_Genie/server/results/urls.py
+++ Lottery_Genie/server/results/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('fetch', views.results, name='results_func'),
     path('fetch/summary/<category>', views.summary, name='summary_func'),
     path("delete", views.delete, name="delete_data"),
-    path("check-combinations/<category>", views.combinations, name="combinations"),
+    path("check-combinations", views.combinations, name="combinations"),
 ]
