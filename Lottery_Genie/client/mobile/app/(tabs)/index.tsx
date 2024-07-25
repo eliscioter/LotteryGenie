92ce8768f494ab310e0e51d64f3e59a5aadd01e9
eliscioter~ <attila433lehun@gmail.com>
2024-07-25 23:54:15 +0800

    feature: create callback function for passing API response

--- Lottery_Genie/client/mobile/app/(tabs)/index.tsx
+++ Lottery_Genie/client/mobile/app/(tabs)/index.tsx
@@ -3,16 +3,29 @@ import CurrentJackpotPrize from "@/components/CurrentJackpotPrize";
 import InputForm from "@/components/InputForm";
 import Result from "@/components/Result";
 import { Text, View } from "@/components/Themed";
+import { useCurrentResultStore } from "@/services/shared/result";
+import { LottoDetails } from "@/types/results-type";
+import { useState } from "react";
 import { ScrollView } from "react-native";
 
 export default function TabOneScreen() {
+  const { setResult } = useCurrentResultStore();
+  const [input_combination, setInputCombination] = useState<string[]>(["", "", "", "", "", ""]);
+
+  const handleFetchResponse = (data: LottoDetails) => {
+    setResult(data);
+  }
+
+  const handleInputCombination = (data: string[]) => {
+    setInputCombination(data);
+  }
   return (
     <ScrollView style={index_styles.container}>
       <Text style={index_styles.title}>{new Date().toLocaleDateString()}</Text>
       <CurrentJackpotPrize />
       <View style={index_styles.input_form_container}>
-        <InputForm />
-        <Result />
+        <InputForm input={handleInputCombination} result_data={handleFetchResponse} />
+        <Result input={input_combination} />
       </View>
     </ScrollView>
   );
