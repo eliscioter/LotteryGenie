fc21bdf369771d0c6342ee3980deb3db54c3cefd
eliscioter~ <attila433lehun@gmail.com>
2024-07-25 23:52:22 +0800

    feature: make result component dynamic

--- Lottery_Genie/client/mobile/components/Result.tsx
+++ Lottery_Genie/client/mobile/components/Result.tsx
@@ -1,34 +1,55 @@
 import Colors from "@/constants/Colors";
 import { View, Text } from "./Themed";
 import { index_styles } from "@/assets/stylesheets/index";
+import { useCurrentResultStore } from "@/services/shared/result";
 
-export default function Result() {
+export default function Result({ input }: { input: string[] }) {
+  const { result } = useCurrentResultStore();
+
+  const numbersGotRight = result?.combination.filter((num) =>
+    input.includes(num)
+  ).length;
+
+  const responseMessage = () => {
+    switch (numbersGotRight) {
+      case 6:
+        return "Congratulations, You won the 1st prize!";
+      case 5:
+        return "Congratulations, You won the 2nd prize!";
+      case 4:
+        return "Congratulations, You won the 3rd prize!";
+      case 3:
+        return "Congratulations, You won the 4th prize!";
+      default:
+        return "Sorry, You lost!";
+    }
+  };
+
   return (
     <View style={index_styles.result_container}>
-      <Text style={{ color: Colors.light_grey, textAlign: "center"}}>
-        You got 5 out of 6. Congratulations,You won!
-      </Text>
-      <View style={index_styles.winning_combinations_container}>
-        <View style={index_styles.correct_number_container}>
-          <Text style={index_styles.white_text}>10</Text>
-        </View>
-        <View style={index_styles.correct_number_container}>
-          <Text style={index_styles.white_text}>10</Text>
-        </View>
-        <View style={index_styles.correct_number_container}>
-          <Text style={index_styles.white_text}>10</Text>
-        </View>
-        <View style={index_styles.correct_number_container}>
-          <Text style={index_styles.white_text}>10</Text>
-        </View>
-        <View style={index_styles.incorrect_number_container}>
-          <Text style={index_styles.white_text}>10</Text>
-        </View>
-        <View style={index_styles.correct_number_container}>
-          <Text style={index_styles.white_text}>10</Text>
-        </View>
-      </View>
-      <Text style={index_styles.prize_won_text}>You won {'\u20B1'} 15,000,000</Text>
+      {input[0] && (
+        <>
+          <Text style={{ color: Colors.light_grey, textAlign: "center" }}>
+            You got {numbersGotRight} out of 6
+          </Text>
+          <Text style={{ color: Colors.light_grey, textAlign: "center" }}>{responseMessage()}</Text>
+          <View style={index_styles.winning_combinations_container}>
+            {result?.combination.map((num, index) => (
+              <View
+                key={index}
+                style={
+                  input.some((inputNum) => inputNum === num)
+                    ? index_styles.correct_number_container
+                    : index_styles.incorrect_number_container
+                }
+              >
+                <Text style={index_styles.white_text}>{num}</Text>
+              </View>
+            ))}
+          </View>
+          <Text style={index_styles.prize_won_text}>You won ₱ 15,000,000</Text>
+        </>
+      )}
     </View>
   );
 }
