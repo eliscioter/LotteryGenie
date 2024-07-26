2bb4c0b92b8265570512823a7977f2d5bcbf69c0
eliscioter~ <attila433lehun@gmail.com>
2024-07-26 22:15:48 +0800

    feature: add types

--- Lottery_Genie/client/mobile/validators/current-results.ts
+++ Lottery_Genie/client/mobile/validators/current-results.ts
@@ -4,13 +4,13 @@ export const lotteryResultSchema = z.object({
   id: z.number().positive(),
   date: z.string().date(),
   category: z.string(),
-  combination: z
-    .string()
-    .regex(/^\d{3}-\d{3}-\d{3}$/)
-    .or(z.string().regex(/^\d{6}$/))
-    .transform((str) => str.trim()),
+  combination: z.array(z.string().min(1).max(2)).length(6),
   prize: z.string(),
-  winners: z.string(),
+  result: z.object({
+    number: z.array(z.string().min(1).max(2)).length(6),
+    count: z.number().int().positive(),
+  }),
+  prize_amount: z.string()
 });
 
 export const InputSchema = z.object({
