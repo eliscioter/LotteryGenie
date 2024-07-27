1786cfa9124cd8a23474c0e6d988d145a6ea7090
eliscioter~ <attila433lehun@gmail.com>
2024-07-27 21:33:15 +0800

    fix: remove ScrollView causing break windowing

--- Lottery_Genie/client/mobile/app/(tabs)/index.tsx
+++ Lottery_Genie/client/mobile/app/(tabs)/index.tsx
@@ -3,13 +3,9 @@ import CurrentJackpotPrize from "@/components/CurrentJackpotPrize";
 import InputForm from "@/components/InputForm";
 import Result from "@/components/Result";
 import { Text, View } from "@/components/Themed";
-import { createContext, useMemo, useState } from "react";
-import { ScrollView } from "react-native";
-
-export const CombCtx = createContext({
-  input_combination: ["", "", "", "", "", ""],
-  setInputCombination: (input_combination: string[]) => {},
-});
+import { CombCtx } from "@/services/shared/user-comb-ctx";
+import { useMemo, useState } from "react";
+import { SafeAreaView, ScrollView } from "react-native";
 
 export default function TabOneScreen() {
   const [input_combination, setInputCombination] = useState<string[]>([
@@ -22,20 +18,29 @@ export default function TabOneScreen() {
   ]);
 
   const update_input_comb = useMemo(
-    () => ({ input_combination, setInputCombination }),
+    () => ({
+      input_combination,
+      setInputCombination,
+      clearInputCombination: () =>
+        setInputCombination(["", "", "", "", "", ""]),
+    }),
     [input_combination]
   );
 
   return (
-    <ScrollView style={index_styles.container}>
-      <Text style={index_styles.title}>{new Date().toLocaleDateString()}</Text>
-      <CurrentJackpotPrize />
-      <View style={index_styles.input_form_container}>
-        <CombCtx.Provider value={update_input_comb}>
-          <InputForm />
-          <Result />
-        </CombCtx.Provider>
+    <SafeAreaView>
+      <View style={index_styles.container}>
+        <Text style={index_styles.title}>
+          {new Date().toLocaleDateString()}
+        </Text>
+        <CurrentJackpotPrize />
+        <View style={index_styles.input_form_container}>
+          <CombCtx.Provider value={update_input_comb}>
+            <InputForm />
+            <Result />
+          </CombCtx.Provider>
+        </View>
       </View>
-    </ScrollView>
+    </SafeAreaView>
   );
 }
