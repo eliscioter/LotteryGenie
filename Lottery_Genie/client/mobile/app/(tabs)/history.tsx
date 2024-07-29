907095ac33173fba13e70b66628b9a5adc42c03d
eliscioter~ <attila433lehun@gmail.com>
2024-07-29 20:04:23 +0800

    feature: initial history page

--- Lottery_Genie/client/mobile/app/(tabs)/history.tsx
+++ Lottery_Genie/client/mobile/app/(tabs)/history.tsx
@@ -1,31 +1,10 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/CurrentJackpotPrize';
 import { Text, View } from '@/components/Themed';
+import { index_styles } from '@/assets/stylesheets/index';
 
 export default function TabTwoScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+    <View style={index_styles.container}>
+      <Text>Hello History</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
---
+++ Lottery_Genie/client/mobile/assets/stylesheets/history/history.ts
@@ -0,0 +1,4 @@
+import { StyleSheet } from "react-native"
+export const history_styles = StyleSheet.create({
+    
+});
