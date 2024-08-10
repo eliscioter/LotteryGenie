f324d5f6c926eaf7453d8524b9f248a5939ded51
eliscioter~ <attila433lehun@gmail.com>
2024-08-10 23:33:03 +0800

    feat: add styles for history screen

--- Lottery_Genie/client/mobile/assets/stylesheets/history/history.ts
+++ Lottery_Genie/client/mobile/assets/stylesheets/history/history.ts
@@ -1,4 +1,14 @@
-import { StyleSheet } from "react-native"
+import Colors from "@/constants/Colors";
+import { StyleSheet } from "react-native";
 export const history_styles = StyleSheet.create({
-    
+  item_container: {
+    backgroundColor: Colors.light_blue_green,
+    marginBottom: 10,
+    marginHorizontal: 10,
+    borderRadius: 10,
+    padding: 10,
+  },
+  item_text_color: {
+    color: Colors.white,
+  },
 });
