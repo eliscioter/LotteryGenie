40d3bd795dacbc54fcdc1561a3134fbb368d063e
eliscioter~ <attila433lehun@gmail.com>
2024-08-10 23:30:50 +0800

    feat: add type for local db items

--- Lottery_Genie/client/mobile/types/results-type.ts
+++ Lottery_Genie/client/mobile/types/results-type.ts
@@ -8,3 +8,10 @@ export type ResultsType = {
 };
 
 export type LottoDetails = z.infer<typeof InputSchema>;
+
+export type LottoCombination = {
+    id: string;
+    category: string;
+    combination: string;
+    input_date: string;
+};
