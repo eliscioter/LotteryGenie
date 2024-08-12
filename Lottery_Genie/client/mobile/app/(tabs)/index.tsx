fdfde20ff576783c47e2c29ef918d6763b10b4a3
eliscioter~ <attila433lehun@gmail.com>
2024-08-12 20:28:17 +0800

    fix: change date format

--- Lottery_Genie/client/mobile/app/(tabs)/index.tsx
+++ Lottery_Genie/client/mobile/app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import Result from "@/components/index/Result";
 import { Text, View } from "@/components/Themed";
 import { CombCtx } from "@/services/shared/user-comb-ctx";
 import { useMemo, useState } from "react";
-import { SafeAreaView, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native";
 
 export default function TabOneScreen() {
   const [input_combination, setInputCombination] = useState<string[]>([
@@ -27,12 +27,19 @@ export default function TabOneScreen() {
     [input_combination]
   );
 
+  const today = new Date();
+
+  const date_today = today.toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
   return (
     <SafeAreaView>
       <View style={index_styles.container}>
-        <Text style={index_styles.title}>
-          {new Date().toLocaleDateString()}
-        </Text>
+        <Text style={index_styles.title}>{date_today}</Text>
         <CurrentJackpotPrize />
         <View style={index_styles.input_form_container}>
           <CombCtx.Provider value={update_input_comb}>
