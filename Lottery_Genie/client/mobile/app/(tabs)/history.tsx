d7f39ec7d6ee6e09b8f2a34af26a795c554c9600
eliscioter~ <attila433lehun@gmail.com>
2024-08-31 12:58:50 +0800

    feat: add select one or many items to delete

--- Lottery_Genie/client/mobile/app/(tabs)/history.tsx
+++ Lottery_Genie/client/mobile/app/(tabs)/history.tsx
@@ -9,14 +9,22 @@ import { FlatList, SafeAreaView } from "react-native";
 import { UpdateHistoryDetailsCtx } from "@/services/shared/history-details-ctx";
 import { Swipeable } from "react-native-gesture-handler";
 import { FontAwesome } from "@expo/vector-icons";
+import { SelectCtx } from "./_layout";
+import { Checkbox } from "react-native-paper";
 
-export default function TabTwoScreen() {
+export default function HistoryScreen() {
   const db = useSQLiteContext();
   const [history, setHistory] = useState<LottoCombination[]>([]);
 
   const { update_history_details, setUpdateHistoryDetails } = useContext(
     UpdateHistoryDetailsCtx
   );
+
+  const { is_pressed } = useContext(SelectCtx);
+
+  const [is_selected_all, setIsSelectedAll] = useState(false);
+
+  const [selected, setSelected] = useState<LottoCombination[]>([]);
 
   useEffect(() => {
     try {
@@ -30,48 +38,103 @@ export default function TabTwoScreen() {
     }
   }, [update_history_details]);
 
+  useEffect(() => {
+    if (is_selected_all) {
+      setSelected(history);
+    } else {
+      setSelected([]);
+    }
+  }, [is_selected_all]);
   return (
     <SafeAreaView style={index_styles.container}>
+      {is_pressed && (
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            backgroundColor: "transparent",
+            paddingBottom: 10,
+          }}
+        >
+          <Checkbox
+            status={is_selected_all ? "checked" : "unchecked"}
+            onPress={() => {
+              setIsSelectedAll(!is_selected_all);
+
+            }}
+            uncheckedColor="white"
+            color="white"
+          />
+          <Text style={{ color: "white", marginLeft: 5 }}>Delete All</Text>
+        </View>
+      )}
       <FlatList
         data={history}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <Swipeable
             renderRightActions={() => (
-              <View style={{
-                justifyContent: "center",
-                alignItems: "center",
-                width: 100,
-                height: "100%",
-                backgroundColor: "red"
-              }}>
-                <FontAwesome 
-                  name="trash"
-                  color={"white"}
-                  size={35}
-                />
+              <View
+                style={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: 100,
+                  height: "100%",
+                  backgroundColor: "red",
+                }}
+              >
+                <FontAwesome name="trash" color={"white"} size={35} />
               </View>
             )}
-            
-            onSwipeableOpen={
-              () => {
-                // ! this is just deleting the state, not the actual data
-                console.info("Deleting item from history", item);
-                const updatedHistory = history.filter((h) => h.id !== item.id);
-                setHistory(updatedHistory);
-              }
-            }
+            onSwipeableOpen={() => {
+              // ! this is just deleting the state, not the actual data
+              console.info("Deleting item from history", item);
+              const updatedHistory = history.filter((x) => x.id !== item.id);
+              setHistory(updatedHistory);
+            }}
           >
-            <View style={history_styles.item_container}>
-              <Text style={history_styles.item_text_color}>
-                Game: {item.category}
-              </Text>
-              <Text style={history_styles.item_text_color}>
-                Combination: {item.combination}
-              </Text>
-              <Text style={history_styles.item_text_color}>
-                Picked Date: {item.input_date}
-              </Text>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                backgroundColor: "transparent",
+              }}
+            >
+              {is_pressed && (
+                <Checkbox
+                  key={item.id}
+                  status={selected.includes(item) ? "checked" : "unchecked"}
+                  uncheckedColor="white"
+                  onPress={() => {
+                    setSelected((prev) => {
+                      if (prev.includes(item)) {
+                        return prev.filter((x) => x !== item);
+                      } else {
+                        return [...prev, item];
+                      }
+                    });
+                  }}
+                  color="white"
+                />
+              )}
+              <View
+                style={{
+                  ...history_styles.item_container,
+                  width: is_pressed ? "85%" : "95%",
+                }}
+              >
+                <Text style={history_styles.item_text_color}>
+                  Game: {item.category}
+                </Text>
+                <Text style={history_styles.item_text_color}>
+                  Combination: {item.combination}
+                </Text>
+                <Text style={history_styles.item_text_color}>
+                  Picked Date: {item.input_date}
+                </Text>
+              </View>
             </View>
           </Swipeable>
         )}
