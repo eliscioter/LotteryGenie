3a154e0a36e81b1266297fc1efeef408b619a2ec
eliscioter~ <attila433lehun@gmail.com>
2024-09-02 22:16:49 +0800

    fix: make delete function functional

--- Lottery_Genie/client/mobile/app/(tabs)/history.tsx
+++ Lottery_Genie/client/mobile/app/(tabs)/history.tsx
@@ -3,14 +3,14 @@ import { index_styles } from "@/assets/stylesheets/index";
 import { useSQLiteContext } from "expo-sqlite";
 import { useContext, useEffect, useState } from "react";
 import { LottoCombination } from "@/types/results-type";
-import { fetchHistory } from "@/services/db/lotto-combinations";
+import { db_table, fetchHistory } from "@/services/db/lotto-combinations";
 import { history_styles } from "@/assets/stylesheets/history/history";
-import { FlatList, SafeAreaView } from "react-native";
+import { FlatList, SafeAreaView, TouchableOpacity } from "react-native";
 import { UpdateHistoryDetailsCtx } from "@/services/shared/history-details-ctx";
 import { Swipeable } from "react-native-gesture-handler";
 import { FontAwesome } from "@expo/vector-icons";
 import { SelectCtx } from "./_layout";
-import { Checkbox } from "react-native-paper";
+import { Checkbox, Modal, Portal } from "react-native-paper";
 
 export default function HistoryScreen() {
   const db = useSQLiteContext();
@@ -26,6 +26,9 @@ export default function HistoryScreen() {
 
   const [selected, setSelected] = useState<LottoCombination[]>([]);
 
+  const [modal_visible, setModalVisibility] = useState(false);
+  const [delete_items, setDeleteItems] = useState(false);
+
   useEffect(() => {
     try {
       fetchHistory(db).then((results) => {
@@ -34,9 +37,26 @@ export default function HistoryScreen() {
     } catch (error) {
       console.error("Error fetching history", error);
     } finally {
-      setUpdateHistoryDetails(false);
+      setUpdateHistoryDetails(history);
     }
   }, [update_history_details]);
+
+  function deleteItems() {
+    try {
+      selected.map(async (item) => {
+        deleteFromState(item);
+        await db.runAsync(`DELETE FROM ${db_table} WHERE id = ?`, item.id);
+      });
+    } catch (error) {
+      console.error("Error deleting items", error);
+    } finally {
+      setUpdateHistoryDetails(history);
+      setDeleteItems(false);
+    }
+  }
+  useEffect(() => {
+    deleteItems();
+  }, [delete_items]);
 
   useEffect(() => {
     if (is_selected_all) {
@@ -45,6 +65,16 @@ export default function HistoryScreen() {
       setSelected([]);
     }
   }, [is_selected_all]);
+
+  function deleteFromState(item: LottoCombination) {
+    try {
+      setHistory((prev) => {
+        return prev.filter((x) => x !== item);
+      });
+    } catch (error) {
+      console.error("Error deleting from state", error);
+    }
+  }
   return (
     <SafeAreaView style={index_styles.container}>
       {is_pressed && (
@@ -54,21 +84,100 @@ export default function HistoryScreen() {
             flexDirection: "row",
             alignItems: "center",
             backgroundColor: "transparent",
+            justifyContent: "space-between",
             paddingBottom: 10,
           }}
         >
-          <Checkbox
-            status={is_selected_all ? "checked" : "unchecked"}
-            onPress={() => {
-              setIsSelectedAll(!is_selected_all);
-
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              backgroundColor: "transparent",
             }}
-            uncheckedColor="white"
-            color="white"
-          />
-          <Text style={{ color: "white", marginLeft: 5 }}>Delete All</Text>
+          >
+            <Checkbox
+              status={is_selected_all ? "checked" : "unchecked"}
+              onPress={() => {
+                console.log("pressing");
+                setIsSelectedAll(!is_selected_all);
+              }}
+              uncheckedColor="white"
+              color="white"
+            />
+            <Text style={{ color: "white", marginLeft: 5 }}>Select All</Text>
+          </View>
+          {selected.length > 0 && (
+            <FontAwesome
+              name="trash"
+              color={"white"}
+              size={25}
+              onPress={() => {
+                setModalVisibility(true);
+              }}
+              style={{ paddingRight: 10 }}
+            />
+          )}
         </View>
       )}
+      <Portal>
+        <Modal
+          visible={modal_visible}
+          onDismiss={() => {
+            setModalVisibility(false);
+          }}
+          contentContainerStyle={{
+            backgroundColor: "white",
+            padding: 20,
+            margin: 20,
+            borderRadius: 10,
+          }}
+        >
+          <Text style={{ textAlign: "center", fontSize: 20 }}>Delete</Text>
+          <Text style={{ textAlign: "center" }}>
+            Are you sure you want to delete these items?
+          </Text>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              marginTop: 10,
+            }}
+          >
+            <TouchableOpacity
+              onPress={() => {
+                setDeleteItems(true);
+                setModalVisibility(false);
+              }}
+              style={{
+                marginRight: 10,
+                borderWidth: 1,
+                borderColor: "green",
+                borderRadius: 50,
+                paddingVertical: 5,
+                paddingHorizontal: 15,
+              }}
+            >
+              <Text>Yes</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                setModalVisibility(false);
+              }}
+              style={{
+                borderWidth: 1,
+                borderColor: "red",
+                borderRadius: 50,
+                paddingVertical: 5,
+                paddingHorizontal: 15,
+              }}
+            >
+              <Text>No</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      </Portal>
       <FlatList
         data={history}
         keyExtractor={(item) => item.id}
@@ -88,10 +197,14 @@ export default function HistoryScreen() {
               </View>
             )}
             onSwipeableOpen={() => {
-              // ! this is just deleting the state, not the actual data
-              console.info("Deleting item from history", item);
-              const updatedHistory = history.filter((x) => x.id !== item.id);
-              setHistory(updatedHistory);
+              setSelected((prev) => {
+                if (prev.includes(item)) {
+                  return prev.filter((x) => x !== item);
+                } else {
+                  return [...prev, item];
+                }
+              });
+              setModalVisibility(true);
             }}
           >
             <View
