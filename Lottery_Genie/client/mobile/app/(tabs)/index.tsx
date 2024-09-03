dec47212079a950bd0c80f493398a65c41ef4613
eliscioter~ <attila433lehun@gmail.com>
2024-09-03 20:46:08 +0800

    feat: add scrollview for keyboard blocking issue

--- Lottery_Genie/client/mobile/app/(tabs)/index.tsx
+++ Lottery_Genie/client/mobile/app/(tabs)/index.tsx
@@ -3,9 +3,11 @@ import CurrentJackpotPrize from "@/components/index/CurrentJackpotPrize";
 import InputForm from "@/components/index/InputForm";
 import Result from "@/components/index/Result";
 import { Text, View } from "@/components/Themed";
+import Colors from "@/constants/Colors";
 import { CombCtx } from "@/services/shared/user-comb-ctx";
 import { useMemo, useState } from "react";
 import { SafeAreaView } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function TabOneScreen() {
   const [input_combination, setInputCombination] = useState<string[]>([
@@ -38,16 +40,23 @@ export default function TabOneScreen() {
 
   return (
     <SafeAreaView>
-      <View style={index_styles.container}>
-        <Text style={index_styles.title}>{date_today}</Text>
-        <CurrentJackpotPrize />
-        <View style={index_styles.input_form_container}>
-          <CombCtx.Provider value={update_input_comb}>
-            <InputForm />
-            <Result />
-          </CombCtx.Provider>
+      <ScrollView
+        style={{
+          backgroundColor: `${Colors.light_blue_green}`,
+          height: "100%",
+        }}
+      >
+        <View style={index_styles.container}>
+          <Text style={index_styles.title}>{date_today}</Text>
+          <CurrentJackpotPrize />
+          <View style={index_styles.input_form_container}>
+            <CombCtx.Provider value={update_input_comb}>
+              <InputForm />
+              <Result />
+            </CombCtx.Provider>
+          </View>
         </View>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
