62e9a480d3b465f32ac5a3825723df788a1be317
eliscioter~ <attila433lehun@gmail.com>
2024-09-04 23:39:17 +0800

    feat: add type

--- Lottery_Genie/client/mobile/services/shared/user-comb-ctx.ts
+++ Lottery_Genie/client/mobile/services/shared/user-comb-ctx.ts
@@ -1,7 +1,13 @@
 import { createContext } from "react";
 
-export const CombCtx = createContext({
-  input_combination: [{ value: "" }],
-  setInputCombination: (input_combination: {value: string}[]) => {},
+export interface CombCtxType {
+  input_combination: { value: string }[];
+  setInputCombination: (input_combination: { value: string }[]) => void;
+  clearInputCombination: () => void;
+}
+
+export const CombCtx = createContext<CombCtxType>({
+  input_combination: [],
+  setInputCombination: (input_combination: { value: string }[]) => {},
   clearInputCombination: () => {},
 });
