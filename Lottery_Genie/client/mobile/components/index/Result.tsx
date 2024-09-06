bd85d177359e7f124827f24374acdf20beb9da84
eliscioter~ <attila433lehun@gmail.com>
2024-09-07 01:33:26 +0800

    fix: make result display multiple combinations

--- Lottery_Genie/client/mobile/components/index/Result.tsx
+++ Lottery_Genie/client/mobile/components/index/Result.tsx
@@ -2,61 +2,46 @@ import Colors from "@/constants/Colors";
 import { View, Text } from "../Themed";
 import { index_styles } from "@/assets/stylesheets/index";
 import { useCurrentResultStore } from "@/services/shared/result";
-import { useContext } from "react";
-import { CombCtx } from "@/services/shared/user-comb-ctx";
+import { Fragment, ReactNode } from "react";
 
 export default function Result() {
   const { result } = useCurrentResultStore();
-  const { input_combination } = useContext(CombCtx);
 
-  // TODO: Transfer this function to the server side
-  const responseMessage = () => {
-    switch (result?.data?.result.count) {
-      case 6:
-        return "Congratulations, You won the 1st prize!";
-      case 5:
-        return "Congratulations, You won the 2nd prize!";
-      case 4:
-        return "Congratulations, You won the 3rd prize!";
-      case 3:
-        return "Congratulations, You won the 4th prize!";
-      default:
-        return "Sorry, You lost!";
-    }
+  const data = result?.data;
+
+  const displayResult = (index: number): ReactNode => {
+    return data?.at(index)?.result.user_combination?.map((val, index_inner) => (
+      <View
+        key={index_inner}
+        style={
+          data.at(index)?.combination.includes(val)
+            ? index_styles.correct_number_container
+            : index_styles.incorrect_number_container
+        }
+      >
+        <Text style={index_styles.white_text}>{val}</Text>
+      </View>
+    ));
   };
 
   return (
     <View style={index_styles.result_container}>
-      {input_combination[0].value && (
-        <>
-          <Text style={{ color: Colors.light_grey, textAlign: "center" }}>
-            You got {result?.result.count} out of 6
-          </Text>
-          <Text style={{ color: Colors.light_grey, textAlign: "center" }}>
-            {responseMessage()}
-          </Text>
-          <View style={index_styles.winning_combinations_container}>
-            {result?.combination.map((num, index) => (
-              <View
-                key={index}
-                style={
-                  input_combination.some((inputNum) => inputNum.value === num)
-                    ? index_styles.correct_number_container
-                    : index_styles.incorrect_number_container
-                }
-              >
-                <Text style={index_styles.white_text}>{num}</Text>
-              </View>
-            ))}
-          </View>
-          // TODO: Transfer this to the server side
-          {result?.result && result?.result.count >= 3 && (
-            <Text style={index_styles.prize_won_text}>
-              You won {result?.prize_amount}
+      {data &&
+        data.length > 0 &&
+        data?.map((value, index) => (
+          <Fragment key={index}>
+            <Text style={{ color: Colors.light_grey, textAlign: "center" }}>
+              You got {value.result.count} out of 6
             </Text>
-          )}
-        </>
-      )}
+
+            <Text style={{ color: Colors.light_grey, textAlign: "center" }}>
+              {value.prize_amount.message}
+            </Text>
+            <View style={index_styles.winning_combinations_container}>
+              {displayResult(index)}
+            </View>
+          </Fragment>
+        ))}
     </View>
   );
 }
