9dda423705d1873db729e0b1940f7bea180a0bd9
eliscioter~ <attila433lehun@gmail.com>
2024-09-09 21:36:43 +0800

    feat: save template

--- Lottery_Genie/client/mobile/app/(tabs)/history.tsx
+++ Lottery_Genie/client/mobile/app/(tabs)/history.tsx
@@ -5,12 +5,18 @@ import { useContext, useEffect, useState } from "react";
 import { LottoCombination } from "@/types/results-type";
 import { db_table, fetchHistory } from "@/services/db/lotto-combinations";
 import { history_styles } from "@/assets/stylesheets/history/history";
-import { FlatList, SafeAreaView, TouchableOpacity } from "react-native";
+import {
+  FlatList,
+  Pressable,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 import { UpdateHistoryDetailsCtx } from "@/services/shared/history-details-ctx";
 import { Swipeable } from "react-native-gesture-handler";
 import { FontAwesome } from "@expo/vector-icons";
 import { SelectCtx } from "./_layout";
 import { Checkbox, Modal, Portal } from "react-native-paper";
+import { set } from "zod";
 
 export default function HistoryScreen() {
   const db = useSQLiteContext();
@@ -27,7 +33,10 @@ export default function HistoryScreen() {
   const [selected, setSelected] = useState<LottoCombination[]>([]);
 
   const [modal_visible, setModalVisibility] = useState(false);
+  const [template_modal_visible, setTemplateModalVisibility] = useState(false);
   const [delete_items, setDeleteItems] = useState(false);
+  const [use_items, setUseItems] = useState(false);
+  const [items_to_use, setItemsToUse] = useState<LottoCombination | null>(null);
 
   useEffect(() => {
     try {
@@ -58,6 +67,22 @@ export default function HistoryScreen() {
     deleteItems();
   }, [delete_items]);
 
+  function useItems(items: LottoCombination | null) {
+    try {
+      if (items) {
+        console.log("using items");
+        setUpdateHistoryDetails([items]);
+      }
+    } catch (error) {
+      console.error("Error using items", error);
+    }
+  }
+
+  useEffect(() => {
+    console.log("here")
+    useItems(items_to_use);
+  }, [use_items]);
+
   useEffect(() => {
     if (is_selected_all) {
       setSelected(history);
@@ -80,15 +105,32 @@ export default function HistoryScreen() {
     }
   }
 
+  function swipeRightAction(item: LottoCombination) {
+    setSelected((prev) => {
+      if (prev.includes(item)) {
+        return prev.filter((x) => x !== item);
+      } else {
+        return [...prev, item];
+      }
+    });
+    setModalVisibility(true);
+  }
+
+  function swipeLeftAction(item: LottoCombination) {
+    console.log("swiped left", item);
+    setTemplateModalVisibility(true);
+    setItemsToUse(item);
+  }
+
   return (
     <SafeAreaView style={index_styles.container}>
-      {
-        history.length === 0 && (
-          <View style={{backgroundColor: "transparent"}}>
-            <Text style={{textAlign: "center", color: "white"}}>No history</Text>
-          </View>
-        )
-      }
+      {history.length === 0 && (
+        <View style={{ backgroundColor: "transparent" }}>
+          <Text style={{ textAlign: "center", color: "white" }}>
+            No history
+          </Text>
+        </View>
+      )}
       {is_pressed && (
         <View
           style={{
@@ -183,6 +225,67 @@ export default function HistoryScreen() {
             <TouchableOpacity
               onPress={() => {
                 setModalVisibility(false);
+                setSelected([]);
+              }}
+              style={{
+                borderWidth: 1,
+                borderColor: "red",
+                borderRadius: 50,
+                paddingVertical: 5,
+                paddingHorizontal: 15,
+              }}
+            >
+              <Text>No</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      </Portal>
+      {/* Use template modal */}
+      <Portal>
+        <Modal
+          visible={template_modal_visible}
+          onDismiss={() => {
+            setTemplateModalVisibility(false);
+          }}
+          contentContainerStyle={{
+            backgroundColor: "white",
+            padding: 20,
+            margin: 20,
+            borderRadius: 10,
+          }}
+        >
+          <Text style={{ textAlign: "center", fontSize: 20 }}>Template</Text>
+          <Text style={{ textAlign: "center" }}>
+            Do you want to use this template?
+          </Text>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              marginTop: 10,
+            }}
+          >
+            <TouchableOpacity
+              onPress={() => {
+                setTemplateModalVisibility(false);
+                setUseItems(true);
+              }}
+              style={{
+                marginRight: 10,
+                borderWidth: 1,
+                borderColor: "green",
+                borderRadius: 50,
+                paddingVertical: 5,
+                paddingHorizontal: 15,
+              }}
+            >
+              <Text>Yes</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                setTemplateModalVisibility(false);
+                setUpdateHistoryDetails([]);
               }}
               style={{
                 borderWidth: 1,
@@ -202,28 +305,25 @@ export default function HistoryScreen() {
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <Swipeable
+            dragOffsetFromLeftEdge={10}
+            dragOffsetFromRightEdge={10}
             renderRightActions={() => (
-              <View
-                style={{
-                  justifyContent: "center",
-                  alignItems: "center",
-                  width: 100,
-                  height: "100%",
-                  backgroundColor: "red",
-                }}
-              >
-                <FontAwesome name="trash" color={"white"} size={35} />
+              <>
+                <View style={history_styles.delete_container}>
+                  <FontAwesome name="trash" color={"white"} size={35} />
+                </View>
+              </>
+            )}
+            renderLeftActions={() => (
+              <View style={history_styles.save_container}>
+                <FontAwesome name="save" color={"white"} size={35} />
               </View>
             )}
-            onSwipeableOpen={() => {
-              setSelected((prev) => {
-                if (prev.includes(item)) {
-                  return prev.filter((x) => x !== item);
-                } else {
-                  return [...prev, item];
-                }
-              });
-              setModalVisibility(true);
+            onSwipeableOpen={(direction) => {
+              console.log("swiped", direction);
+              direction === "right"
+                ? swipeRightAction(item)
+                : swipeLeftAction(item);
             }}
           >
             <View
@@ -251,7 +351,10 @@ export default function HistoryScreen() {
                   color="white"
                 />
               )}
-              <View
+              <Pressable
+                onLongPress={() => {
+                  console.log(item.combination);
+                }}
                 style={{
                   ...history_styles.item_container,
                   width: is_pressed ? "85%" : "95%",
@@ -261,12 +364,15 @@ export default function HistoryScreen() {
                   Game: {item.category}
                 </Text>
                 <Text style={history_styles.item_text_color}>
-                  Combination: {item.combination.replace(/[\[\]"]/g, '').replace(/,\s*/g, ', ')}
+                  Combination:{" "}
+                  {item.combination
+                    .replace(/[\[\]"]/g, "")
+                    .replace(/,\s*/g, ", ")}
                 </Text>
                 <Text style={history_styles.item_text_color}>
                   Picked Date: {item.input_date}
                 </Text>
-              </View>
+              </Pressable>
             </View>
           </Swipeable>
         )}
