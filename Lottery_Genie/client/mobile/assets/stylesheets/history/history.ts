220cb36a91b8dc4f28a96ba0284a2ef3663c96ba
eliscioter~ <attila433lehun@gmail.com>
2024-09-09 21:35:18 +0800

    feat: add styles

--- Lottery_Genie/client/mobile/assets/stylesheets/history/history.ts
+++ Lottery_Genie/client/mobile/assets/stylesheets/history/history.ts
@@ -11,4 +11,24 @@ export const history_styles = StyleSheet.create({
   item_text_color: {
     color: Colors.white,
   },
+  delete_container: {
+    flex: 1,
+    alignItems: "flex-end",
+    justifyContent: "center",
+    backgroundColor: Colors.red,
+    marginBottom: 10,
+    marginHorizontal: 10,
+    borderRadius: 10,
+    padding: 10,
+  },
+  save_container: {
+    flex: 1,
+    alignItems: "flex-start",
+    justifyContent: "center",
+    backgroundColor: Colors.grey,
+    marginBottom: 10,
+    marginHorizontal: 10,
+    borderRadius: 10,
+    padding: 10,
+  }
 });
