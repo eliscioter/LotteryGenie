dd15ba09a5c73d772f7b5a684177b9ace013636b
eliscioter~ <attila433lehun@gmail.com>
2024-09-11 22:42:14 +0800

    fix: change type

--- Lottery_Genie/client/mobile/types/results-type.ts
+++ Lottery_Genie/client/mobile/types/results-type.ts
@@ -12,7 +12,7 @@ export type LottoDetails = z.infer<typeof InputSchema>;
 export type LottoCombination = {
     id: string;
     category: string;
-    combination: string;
+    combination: string[]
     input_date: string;
     created_at: Date;
 };
