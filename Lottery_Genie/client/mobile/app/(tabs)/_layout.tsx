065e899739adbadb3f635ba52167f49bf0c546ad
eliscioter~ <attila433lehun@gmail.com>
2024-10-03 21:38:50 +0800

    chore: formatted

--- Lottery_Genie/client/mobile/app/(tabs)/_layout.tsx
+++ Lottery_Genie/client/mobile/app/(tabs)/_layout.tsx
@@ -7,6 +7,8 @@ import Colors from "@/constants/Colors";
 import { useColorScheme } from "@/components/useColorScheme";
 import { useClientOnlyValue } from "@/components/useClientOnlyValue";
 import { UpdateHistoryDetailsCtx } from "@/services/shared/history-details-ctx";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from ".";
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
@@ -32,83 +34,85 @@ export default function TabLayout() {
 
   return (
     <SelectCtx.Provider value={{ is_pressed, setIsPressed }}>
-      <Tabs
-        screenOptions={{
-          tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-          // Disable the static render of the header on web
-          // to prevent a hydration error in React Navigation v6.
-          headerShown: useClientOnlyValue(false, true),
-          headerStyle: {
-            backgroundColor: Colors.forest_green,
-          },
-          headerTintColor: Colors.white,
-          tabBarStyle: {
-            backgroundColor: Colors.forest_green,
-          },
-        }}
-      >
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: "Lotto",
-            tabBarIcon: ({ color }) => (
-              <TabBarIcon name="gamepad" color={color} />
-            ),
-            headerRight: () => (
-              <Link href="/modal" asChild>
-                <Pressable>
-                  {({ pressed }) => (
-                    <FontAwesome
-                      name="user"
-                      size={25}
-                      color={Colors.white}
-                      style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                    />
-                  )}
-                </Pressable>
-              </Link>
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="history"
-          listeners={{
-            tabPress: () => {
-              setHistoryTabPressed(!history_tab_pressed);
+        <Tabs
+          screenOptions={{
+            tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+            // Disable the static render of the header on web
+            // to prevent a hydration error in React Navigation v6.
+            headerShown: useClientOnlyValue(false, true),
+            headerStyle: {
+              backgroundColor: Colors.forest_green,
+            },
+            headerTintColor: Colors.white,
+            tabBarStyle: {
+              backgroundColor: Colors.forest_green,
             },
           }}
-          options={{
-            title: "History",
-            tabBarIcon: ({ color }) => <TabBarIcon name="book" color={color} />,
-            headerRight: () => {
-              return (
-                update_history_details.length > 0 &&
-                (is_pressed ? (
-                  <Pressable
-                    onPress={() => {
-                      setIsPressed(false);
-                    }}
-                  >
-                    <Text style={{ color: "white", marginRight: 15 }}>
-                      Cancel
-                    </Text>
+        >
+          <Tabs.Screen
+            name="index"
+            options={{
+              title: "Lotto",
+              tabBarIcon: ({ color }) => (
+                <TabBarIcon name="gamepad" color={color} />
+              ),
+              headerRight: () => (
+                <Link href="/modal" asChild>
+                  <Pressable>
+                    {({ pressed }) => (
+                      <FontAwesome
+                        name="user"
+                        size={25}
+                        color={Colors.white}
+                        style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                      />
+                    )}
                   </Pressable>
-                ) : (
-                  <Pressable
-                    onPress={() => {
-                      setIsPressed(true);
-                    }}
-                  >
-                    <Text style={{ color: "white", marginRight: 15 }}>
-                      Select
-                    </Text>
-                  </Pressable>
-                ))
-              );
-            },
-          }}
-        />
-      </Tabs>
+                </Link>
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="history"
+            listeners={{
+              tabPress: () => {
+                setHistoryTabPressed(!history_tab_pressed);
+              },
+            }}
+            options={{
+              title: "History",
+              tabBarIcon: ({ color }) => (
+                <TabBarIcon name="book" color={color} />
+              ),
+              headerRight: () => {
+                return (
+                  update_history_details.length > 0 &&
+                  (is_pressed ? (
+                    <Pressable
+                      onPress={() => {
+                        setIsPressed(false);
+                      }}
+                    >
+                      <Text style={{ color: "white", marginRight: 15 }}>
+                        Cancel
+                      </Text>
+                    </Pressable>
+                  ) : (
+                    <Pressable
+                      onPress={() => {
+                        setIsPressed(true);
+                      }}
+                    >
+                      <Text style={{ color: "white", marginRight: 15 }}>
+                        Select
+                      </Text>
+                    </Pressable>
+                  ))
+                );
+              },
+            }}
+          />
+        </Tabs>
     </SelectCtx.Provider>
   );
 }
