ef177bbdf02046eee768d331037227ce69befaff
eliscioter~ <attila433lehun@gmail.com>
2025-04-26 21:14:55 +0800

    fix: add optional chaining

--- Lottery_Genie/client/mobile/components/index/Result.tsx
+++ Lottery_Genie/client/mobile/components/index/Result.tsx
@@ -10,18 +10,20 @@ export default function Result() {
   const data = result?.data;
 
   const displayResult = (index: number): ReactNode => {
-    return data?.at(index)?.result.user_combination?.map((val, index_inner) => (
-      <View
-        key={index_inner}
-        style={
-          data.at(index)?.combination.includes(val)
-            ? index_styles.correct_number_container
-            : index_styles.incorrect_number_container
-        }
-      >
-        <Text style={index_styles.white_text}>{val}</Text>
-      </View>
-    ));
+    return data
+      ?.at(index)
+      ?.result?.user_combination?.map((val, index_inner) => (
+        <View
+          key={index_inner}
+          style={
+            data.at(index)?.combination.includes(val)
+              ? index_styles.correct_number_container
+              : index_styles.incorrect_number_container
+          }
+        >
+          <Text style={index_styles.white_text}>{val}</Text>
+        </View>
+      ));
   };
 
   return (
@@ -29,19 +31,21 @@ export default function Result() {
       {data &&
         data.length > 0 &&
         data?.map((value, index) => (
-          <Fragment key={index}>
-            <Text style={{ color: Colors.light_grey, textAlign: "center" }}>
-              You got {value.result.count} out of 6
-            </Text>
+            <Fragment key={index}>
+              {value.result?.count != undefined && (
+                <Text style={{ color: Colors.light_grey, textAlign: "center" }}>
+                  You got {value.result?.count} out of 6
+                </Text>
+              )}
 
-            <Text style={{ color: Colors.light_grey, textAlign: "center" }}>
-              {value.prize_amount.message}
-            </Text>
-            <View style={index_styles.winning_combinations_container}>
-              {displayResult(index)}
-            </View>
-          </Fragment>
-        ))}
+              <Text style={{ color: Colors.light_grey, textAlign: "center" }}>
+                {value.prize_amount.message}
+              </Text>
+              <View style={index_styles.winning_combinations_container}>
+                {displayResult(index)}
+              </View>
+            </Fragment>
+          ))}
     </View>
   );
 }
